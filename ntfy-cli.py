d2f8337fba5babe483e8dee9d17b7ecff4cfa500
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-05-29 20:52:20 -0300

    commit

--- ntfy-cli.py
+++ ntfy-cli.py
@@ -26,12 +26,26 @@ NTFY_TOKEN = os.environ.get('NTFY_TOKEN') or ""
 ICON_IMAGE_URL = "https://public.kassius.org/python-logo.png"
 MESSAGE_TITLE = "Sent via ntfy-cli.py"
 MESSAGE_BODY = sys.stdin if NTFY_FROM_STDIN else 'testing\nnotification'
+
 HEADERS = {
         "X-Title": MESSAGE_TITLE,
         "X-Icon": ICON_IMAGE_URL,
         "X-Priority": "urgent",
         "X-Tags": "+1, richtig"
         }
+
+argument_parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        description='Send ntfy notification',
+        epilog="This is the epilog\ngh:iacchus/ntfy-cli"
+        )
+
+argument_parser.add_argument("title")
+argument_parser.add_argument("message")
+argument_parser.add_argument("--markdown", action="store_true")
+args = argument_parser.parse_args()
+print(args, args.message)
+
 #  print(NTFY_SERVER, NTFY_TOKEN, NTFY_TOPIC, NTFY_URL)
 
 #  basic_creds = HTTPBasicAuth("", NTFY_TOKEN)
