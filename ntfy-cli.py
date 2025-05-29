c951d8bc99ebbde833c3b7b9d990d052c6816fbf
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-05-29 19:15:48 -0300

    commit

--- ntfy-cli.py
+++ ntfy-cli.py
@@ -6,13 +6,29 @@ import argparse
 import click
 import requests
 
+from requests.auth import HTTPBasicAuth
+
 # https://docs.ntfy.sh/publish/
 
 NTFY_SERVER = os.environ.get('NTFY_SERVER')
 NTFY_TOPIC = os.environ.get('NTFY_TOPIC')
 #  NTFY_DEFAULT_TOPIC_URL = f"{NTFY_SERVER}/{NTFY_TOPIC}"
 NTFY_URL = f"{NTFY_SERVER}/{NTFY_TOPIC}"
-NTFY_TOKEN = os.environ.get('NTFY_TOKEN')
+NTFY_TOKEN = os.environ.get('NTFY_TOKEN') or ""
 
-requests.post(url=NTFY_URL,
-              data='testing\nnotification')
+ICON_IMAGE_URL = "https://public.kassius.org/python-logo.png"
+
+HEADERS = {
+        "X-Icon": ICON_IMAGE_URL,
+        "X-Priority": "urgent",
+        }
+#  print(NTFY_SERVER, NTFY_TOKEN, NTFY_TOPIC, NTFY_URL)
+
+#  basic_creds = HTTPBasicAuth("", NTFY_TOKEN)
+
+r = requests.post(url=NTFY_URL,
+                  auth=('', NTFY_TOKEN),
+                  data='testing\nnotification',
+                  headers=HEADERS)
+
+print(r.text)
