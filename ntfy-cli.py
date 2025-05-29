ea0cc587d5d1459992908ee920c87a33cfdd9592
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-05-29 18:38:38 -0300

    commit

--- ntfy-cli.py
+++ ntfy-cli.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 
+import os
+import argparse
+
 import click
 import requests
+
+# https://docs.ntfy.sh/publish/
+
+NTFY_SERVER = os.environ.get('NTFY_SERVER')
+NTFY_TOPIC = os.environ.get('NTFY_TOPIC')
+#  NTFY_DEFAULT_TOPIC_URL = f"{NTFY_SERVER}/{NTFY_TOPIC}"
+NTFY_URL = f"{NTFY_SERVER}/{NTFY_TOPIC}"
+NTFY_TOKEN = os.environ.get('NTFY_TOKEN')
+
+requests.post(url=NTFY_URL,
+              data='testing\nnotification')
